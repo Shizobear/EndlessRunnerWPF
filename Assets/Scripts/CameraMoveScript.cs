502a05fa7775fde66abea418e209ecfc9c7bafa8
KTobi <K.Tobi@outlook.de>
2018-07-21 01:02:49 +0200

    added character controls

--- Assets/Scripts/CameraMoveScript.cs
+++ Assets/Scripts/CameraMoveScript.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovementBaseControl : MonoBehaviour {
+public class CameraMoveScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	public GameObject player;
+	public Transform cameraTransform;
+	void Awake () {
+		cameraTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
