9bc2c512f5c9705e2b1301255913e5f91331004a
d-holbach <daniel_holbach@posteo.de>
2018-07-21 18:57:50 +0200

    Highscore

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private ScoreManager theScoreManager;
+
+	// Use this for initialization
+	void Start () {
+		theScoreManager = FindObjectOfType<ScoreManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// public IEnumerator RestartGameCo() {
+	// 	theScoreManager.scoreIncreasing = false;
+
+
+	// 	theScoreManager.scoreCount = 0;
+	// 	theScoreManager.scoreIncreasing = true;
+	// 	return ;
+	// }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text highscoreText;
+	public float scoreCount;
+	public float highscoreCount;
+	public float pointsPerSecond;
+	public bool scoreIncreasing;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey("Highscore")) {
+				highscoreCount = PlayerPrefs.GetFloat("Highscore");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (scoreIncreasing) {
+				scoreCount += pointsPerSecond * Time.deltaTime;
+		}
+		
+		if (scoreCount > highscoreCount) {
+				highscoreCount = scoreCount;
+				PlayerPrefs.SetFloat("Highscore", highscoreCount);
+		}
+
+		scoreText.text = "Score: " + Mathf.Round(scoreCount);
+		highscoreText.text = "Highscore: " + Mathf.Round(highscoreCount);
+	}
+}
