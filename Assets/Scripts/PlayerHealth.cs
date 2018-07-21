5a566fb73eb870058c6e07c4783e68c8236d6539
d-holbach <daniel_holbach@posteo.de>
2018-07-21 15:47:13 +0200

    Health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int currentLives;
+	public Text livesUI;
+
+	void Start() {
+		livesUI.text = "Lives: " + currentLives.ToString();
+	}
+
+	void Update() {
+		livesUI.text = "Lives: " + currentLives.ToString();
+	}
+
+}
