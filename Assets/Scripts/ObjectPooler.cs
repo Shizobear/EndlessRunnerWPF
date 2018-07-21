bc48d8d0429da5a5ac612340548b5f69107bf09c
KTobi <K.Tobi@outlook.de>
2018-07-21 18:24:26 +0200

    added beginning of procgen

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject pooledObject;
+    public int poolSize;
+    List<GameObject> pool;
+    void Start()
+    {
+
+        pool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            GenerateObject();
+        }
+    }
+
+    private void GenerateObject()
+    {
+        GameObject obj = (GameObject)Instantiate(pooledObject);
+        obj.SetActive(false);
+        pool.Add(obj);
+    }
+
+
+public GameObject GetObjectFromPool()
+{
+
+    for (int i = 0; i < pool.Count; i++)
+    {
+        if (pool[i].activeInHierarchy == false)
+        {
+            return pool[i];
+        }
+    }
+
+	GenerateObject();
+	return pool[pool.Count - 1];
+
+}
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distanceBetween;
+    private float platformWidth;
+
+    public float distanceBetweenMin, distanceBetweenMax;
+
+	public ObjectPooler objPooler;
+
+    private void Awake()
+    {
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CreatePlatform();
+
+    }
+
+    private void CreatePlatform()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+			distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            GameObject newPlatform = objPooler.GetObjectFromPool();
+
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive(true);
+        }
+    }
+
+}
